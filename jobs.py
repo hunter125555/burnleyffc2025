c6fc6469d42315bc0af29e9240b0a82932474ee9
Sagar Manohar <sagarmanohar24@gmail.com>
2017-10-18 22:52:59 -0400

    add jobs

--- jobs.py
+++ jobs.py
@@ -3,16 +3,20 @@ from manage import update_live_points, update_for_gw, update_test
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='wed-sun', hour='23', minute='38-43/1', timezone='America/New_York')
-def test_job():
-	update_test()
+# @sched.scheduled_job('cron', day_of_week='wed-sun', hour='23', minute='38-43/1', timezone='America/New_York')
+# def test_job():
+# 	update_test()
 
-# @sched.scheduled_job('cron', day_of_week='sat-sun', hour='7', minute='30', timezone='America/New_York')
-# def update_gw_data():
-# 	update_for_gw()
+@sched.scheduled_job('cron', day_of_week='sat-sun', hour='8-17/3', timezone='America/New_York')
+def update_gw_data():
+	update_for_gw()
 
-# @sched.scheduled_job('cron', day_of_week='sat-sun', hour='8-16', minute='0-59/10', timezone='America/New_York')
-# def update_live():
-# 	update_live_points()
+@sched.scheduled_job('cron', day_of_week='sat-sun', hour='8-14', minute='0-59/10', timezone='America/New_York')
+def update_live():
+	update_live_points()
+
+@sched.scheduled_job('cron', day_of_week='fri', hour='15-19/2', timezone='America/New_York')
+def update_early_gw_data():
+	update_for_gw()
 
 sched.start()
